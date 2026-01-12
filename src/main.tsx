6e5ee7cd1031e86d4b824438beee919fd0615ba1
Dan <dansharabi2003@gmail.com>
2026-01-12 04:19:14 +0200

    Updated App, Index css and Main

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeInit } from "../.flowbite-react/init";
+import Layout from "./Layout/Layout.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeInit />
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </StrictMode>,
 );
 

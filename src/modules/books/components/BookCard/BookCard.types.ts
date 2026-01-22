31ddee9ef65395279889f84bce6daa683c2e0193
Dan <dansharabi2003@gmail.com>
2026-01-22 04:51:42 +0200

    Created BookCard.tsx and BookCard.types.ts

---
+++ src/modules/books/components/BookCard/BookCard.types.ts
@@ -0,0 +1,9 @@
+import type { BookResponse } from "../../api/book.types";
+
+export interface BookCardProps {
+  book: BookResponse; // The core book data model to display.
+
+  onLike: (book: BookResponse) => void; // Sends the book object to the parent to toggle its 'isFavorite' status in the database.
+  onEdit: (book: BookResponse) => void; // Sends the book object to the parent to populate a form with the book's details.
+  onDelete: (book: BookResponse) => void; // Sends the book object to the parent to pull the ID and filter this book out of the list.
+}

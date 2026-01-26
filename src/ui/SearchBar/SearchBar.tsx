cd96a8eaa98824278fcbade72b6dcf6f17d9dc60
Dan <dansharabi2003@gmail.com>
2026-01-26 09:04:42 +0200

    Refactor SearchBar component to include a clear button for input reset

--- src/ui/SearchBar/SearchBar.tsx
+++ src/ui/SearchBar/SearchBar.tsx
@@ -3,10 +3,22 @@ import type { SearchBarProps } from "./SearchBar.types";
 
 export function SearchBar({ value, onChange, placeholder }: SearchBarProps) {
   return (
-    <TextInput
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder={placeholder ?? "Search..."}
-    />
+    <div className="relative w-full">
+      <TextInput
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder ?? "Search..."}
+      />
+      {value && (
+        <button
+          className="absolute top-1/5 right-5 text-gray-400 hover:text-gray-300"
+          onClick={() => onChange("")}
+        >
+          <span color="gray" className="text-xs">
+            CLEAR
+          </span>
+        </button>
+      )}
+    </div>
   );
 }

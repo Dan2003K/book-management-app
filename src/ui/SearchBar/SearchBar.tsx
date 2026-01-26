7673b007d5164cb3f898ee7f40e2d8a07c8fc2ef
Dan <dansharabi2003@gmail.com>
2026-01-26 09:39:07 +0200

    Refactor SearchBar button positioning and styling for improved layout

--- src/ui/SearchBar/SearchBar.tsx
+++ src/ui/SearchBar/SearchBar.tsx
@@ -11,12 +11,12 @@ export function SearchBar({ value, onChange, placeholder }: SearchBarProps) {
       />
       {value && (
         <button
-          className="absolute top-1/5 right-5 text-gray-400 hover:text-gray-300"
+          className="absolute top-1/2 right-1 -translate-y-1/2 text-gray-400 hover:text-gray-300"
           onClick={() => onChange("")}
         >
-          <span color="gray" className="text-xs">
-            CLEAR
-          </span>
+          <div color="gray" className="flex h-full bg-gray-700 px-4">
+            <span className="text-xs">CLEAR</span>
+          </div>
         </button>
       )}
     </div>
